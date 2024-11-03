33c98d9d1023c4b311ac116a6c5ccdcbb5d3e468
sudarshan <kosudarshan15@gmail.com>
2024-11-03 13:11:22 +0530

    l

---
+++ Descriptive-Analysis/app.py
@@ -0,0 +1,45 @@
+from flask import Flask,render_template
+
+import pandas as pd
+
+app = Flask(__name__)
+
+def load_data():
+
+	data_path = '/home/sudarshan/work/Data-Science/Descriptive-Analysis/Data/sales_data.csv'
+
+	data=pd.read_csv(data_path)
+
+	total_sales=data["Sales"].sum()
+
+	avg_sales=data["Sales"].mean()
+
+	total_customer=data["Customers"].sum()
+
+	avg_customer=data["Customers"].mean()
+
+	return{
+
+		"total_sales":total_sales,
+
+		"avg_sales":avg_sales,
+
+		"total_customer":total_customer,
+
+		"avg_customer":avg_customer,
+
+		"records":data.to_dict(orient="records")
+
+	}
+
+@app.route("/")
+
+def index():
+
+	analytics_data=load_data()
+
+	return render_template("index.html",data=analytics_data)
+
+if  __name__=="__main__":
+
+	app.run(debug=True)
